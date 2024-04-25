878ffaaf52eeae3020e3f8ff4d066d04e2b5e502
dir-FY24itotomo <tomo.ito.b503@dirbato.co.jp>
2024-04-25 14:32:33 +0900

    Finish Lesson 21!

---
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -0,0 +1,40 @@
+package com.techacademy.service;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.techacademy.entity.User;
+import com.techacademy.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    /** 全件を検索して返す */
+    public List<User> getUserList() {
+        // リポジトリのfindAllメソッドを呼び出す
+        return userRepository.findAll();
+    }
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+    
+    //User registration
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    /** Userの削除を行なう */
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
+    }
+}
